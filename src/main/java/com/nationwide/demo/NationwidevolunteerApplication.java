036340ba6c3fdcdd4a4d829a49e3460dabbfdf9c
imo58@hotmail.co.uk <imo58@hotmail.co.uk>
2019-10-25 12:27:05 +0100

    First Commit

---
+++ src/main/java/com/nationwide/demo/NationwidevolunteerApplication.java
@@ -0,0 +1,13 @@
+package com.nationwide.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationwidevolunteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationwidevolunteerApplication.class, args);
+	}
+
+}
