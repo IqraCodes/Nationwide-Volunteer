e8378e645809d3514ef0ff84507b5071c1e2a9c5
imo58@hotmail.co.uk <imo58@hotmail.co.uk>
2019-10-27 18:08:28 +0000

    - added some HTML - changes to rest

--- src/main/java/com/nationwide/rest/volunteercontroller.java
+++ src/main/java/com/nationwide/rest/volunteercontroller.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.nationwide.business.volunteerService;
 import com.nationwide.persistence.domain.volunteer;
 
