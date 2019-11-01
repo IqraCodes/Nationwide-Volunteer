96adaac6806063f19a4a761180470c102c063c99
imo58@hotmail.co.uk <imo58@hotmail.co.uk>
2019-11-01 18:10:04 +0000

    -did functionality for delete, post and get

--- src/main/java/com/nationwide/rest/volunteercontroller.java
+++ src/main/java/com/nationwide/rest/volunteercontroller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import com.nationwide.business.volunteerService;
 import com.nationwide.domain.Volunteer;
 
 @RestController
+@CrossOrigin("*")
 
 @RequestMapping("/volunteerapp")
 public class volunteercontroller {
@@ -22,25 +24,26 @@ public class volunteercontroller {
 	@Autowired
 	private volunteerService volunteerService;
 	
+	@CrossOrigin("*")
 	@GetMapping(value="/volunteer")
 	public List<Volunteer> getAllvolunteer(){
 		return volunteerService.getAllvolunteer();
 	}
-	@CrossOrigin
+	@CrossOrigin("*")
 	@PostMapping(value="/volunteer")
 	public Volunteer savevolunteer (@RequestBody Volunteer volunteerToSave) {
 		System.out.println("hello testjng");
 		return volunteerService.savevolunteer(volunteerToSave);
 	}
-	
+	@CrossOrigin("*")
 	@PutMapping(value="/volunteer")
 	public Volunteer updatevolunteer (@RequestBody Volunteer volunteerToUpdate) {
 		return volunteerService.updatevolunteer(volunteerToUpdate);
 	}
-	
+	@CrossOrigin("*")
 	@DeleteMapping(value="/volunteer/{volunteerIdToDelete}")
-	public String deletevolunteer (Long volunteeridToDelete) {
-		return volunteerService.deletevolunteer(volunteeridToDelete);
+	public String deletevolunteer (@PathVariable Long volunteerIdToDelete) {
+		return volunteerService.deletevolunteer(volunteerIdToDelete);
 	}
 	
 }
